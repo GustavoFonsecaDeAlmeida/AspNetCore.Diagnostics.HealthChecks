1efa65dc0166502c774c89c7c54982d6ce3080aa
Gustavo Fonseca <gustavo.fonseca@mundipagg.com>
2020-07-16 13:43:59 -0300

    Add base splunk publisher

---
+++ src/HealthChecks.Publisher.Splunk/DependencyInjection/SplunkHealthCheckBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HealthChecks.Publisher.Splunk.DependencyInjection
+{
+    public static class SplunkHealthCheckBuilderExtensions
+{
+    const string NAME = "seq";
+    /// <summary>
+    ///     Add a health check publisher for Seq.
+    /// </summary>
+    /// <remarks>
+    ///     For each <see cref="HealthReport" /> published a new metric value indicating the health check status (2 Healthy, 1
+    ///     Degraded, 0 Unhealthy)  and the total time the health check took to execute on seconds.
+    /// </remarks>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder" />.</param>
+    /// <param name="setup">The Sql configuration options.</param>
+    /// <param name="name"> The registration name. This is also the associated http client name if you use AddHttpClient </param>
+    /// <returns>The <see cref="IHealthChecksBuilder" />.</returns>
+    public static IHealthChecksBuilder AddSplunkPublisher(this IHealthChecksBuilder builder, Action<SplunkOptions> setup, string name = default)
+    {
+        builder.Services.AddHttpClient();
+
+        var options = new SplunkOptions();
+        setup?.Invoke(options);
+
+        var registrationName = name ?? NAME;
+
+        builder.Services.AddSingleton<IHealthCheckPublisher>(sp =>
+        {
+            return new SplunkPublisher(() => sp.GetRequiredService<IHttpClientFactory>().CreateClient(registrationName), options);
+        });
+
+        return builder;
+    }
+}
+}
---
+++ src/HealthChecks.Publisher.Splunk/SplunkOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthChecks.Publisher.Splunk
+{
+    public class SplunkOptions
+    {
+        public string Endpoint { get; set; }
+        public string Token { get; set; }
+    }
+}
